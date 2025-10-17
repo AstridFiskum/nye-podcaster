79113579145927d6dcbbc5897a27daba91870175
AstridFiskum <astrid_fiskum@hotmail.com>
2025-10-17 15:44:57 +0200

    prøver å få til url

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import svgr from 'vite-plugin-svgr';
+
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
+
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-});
+  plugins: [react()],
+  base: '/nye-podcaster/',
+})
